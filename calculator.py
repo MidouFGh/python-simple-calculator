976c93b13e478b823c8e3839ef611201a0115d78
Hamid Achouri <midouachouri2021@gmail.com>
2024-07-14 09:48:49 +0100

    just a python calculator

---
+++ calculator.py
@@ -0,0 +1,35 @@
+software = True
+while software:
+    #--------------------------------------------calculation----------------------------------------------
+    def calculator():
+        num1 = int(input("number 1: "))
+        mark = input("\nplease enter the mark [+,-,/,*]: ")
+        num2 = int(input("\nnumber 2: "))
+        if mark == "+":
+            result = num1 + num2
+            print(result)
+        elif mark == "-":
+            result = num1 - num2
+            print(result)
+        elif mark == "/":
+            result = num1 / num2
+            print(result)
+        elif mark == "*":
+            result = num1 * num2
+            print(result)
+        else:
+            print("sorry restart the software")
+            exit()
+    calculator()
+    #-----------------------------------------------restart-----------------------------------------------
+    def restart():
+        q1 = input("do you want to exit the software [y/n]: ")
+        if q1 == "y" or "Y" or "Yes" or "yes" or "YES":
+            print("\n")
+            exit()
+        elif q1 == "n" or "N" or "No" or "no" or "NO":
+            calculator()
+        else:
+            print("repeate please !")
+            restart()
+    restart()
